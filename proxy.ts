8328912e3133ffb2c88729234afcb14d61b7ebe1
Vadym Buleziuk <666buzya@gmail.com>
2026-08-01 23:37:40 +0200

    try now

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 import { parseSetCookie } from "cookie";
 import { checkServerSession } from "./lib/api/serverApi";
 
@@ -8,8 +9,10 @@ const publicRoutes = ["/sign-in", "/sign-up"];
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const refreshToken = request.cookies.get("refreshToken")?.value;
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
 
   const isPublicRoute = publicRoutes.some((route) =>
     pathname.startsWith(route),
@@ -21,9 +24,9 @@ export async function proxy(request: NextRequest) {
 
   if (!accessToken) {
     if (refreshToken) {
-      const response = NextResponse.next();
-
       const data = await checkServerSession();
+
+      const response = NextResponse.next();
 
       const setCookie = data.headers["set-cookie"];
 
@@ -47,9 +50,7 @@ export async function proxy(request: NextRequest) {
       }
 
       if (isPublicRoute) {
-        return NextResponse.redirect(new URL("/", request.url), {
-          headers: response.headers,
-        });
+        return NextResponse.redirect(new URL("/", request.url));
       }
 
       if (isPrivateRoute) {
